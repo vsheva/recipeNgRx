ae1a582312121565268294e0ca3f5a2775b1b05d
vsheva <valera.sheva@gmail.com>
2024-02-22 23:12:00 -0500

    ngrx

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
+import { StoreModule } from '@ngrx/store';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    SharedModule,
+    CoreModule,
+    StoreModule.forRoot({}, {}),
+  ],
+  bootstrap: [AppComponent],
+  // providers: [LoggingService]
+})
+export class AppModule {}
