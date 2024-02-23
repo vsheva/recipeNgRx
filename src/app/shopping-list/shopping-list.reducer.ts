f89dbd67c373cc89a9292e96d44db3b7406e3111
vsheva <valera.sheva@gmail.com>
2024-02-22 23:28:26 -0500

    reducer

--- src/app/shopping-list/shopping-list.reducer.ts
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -1,12 +1,21 @@
 import {Ingredient} from "../shared/ingredient.model";
+import {Action} from "@ngrx/store";
+
 
 const initialState = {
-  ingredients:[
+  ingredients: [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
   ]
 };
 
-export function shoppingListReducer(initialState,action) {
-
+export function shoppingListReducer(state = initialState, action: Action) {
+  switch (action.type) {
+    case 'ADD_INGREDIENT':
+      state.ingredients.push()
+      return {
+       ...state,
+        ingredients: [...state.ingredients, action]
+      }
+  }
 }
