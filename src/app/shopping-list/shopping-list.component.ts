32a04bde899796314c970a8d508032a61a384b62
vsheva <valera.sheva@gmail.com>
2024-03-28 23:00:46 -0400

    fix

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -24,12 +24,7 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.ingredients = this.store.select('shoppingList');
-    // this.ingredients = this.slService.getIngredients();
-    // this.subscription = this.slService.ingredientsChanged.subscribe(
-    //   (ingredients: Ingredient[]) => {
-    //     this.ingredients = ingredients;
-    //   }
-    // );
+
 
     this.loggingService.printLog('Hello from ShoppingListComponent ngOnInit!');
   }
